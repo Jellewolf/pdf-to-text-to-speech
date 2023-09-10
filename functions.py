4b3e26c1b94a5076eda740e9689e59ea9e5364d4
Jelle <63313141+Jellewolf@users.noreply.github.com>
2023-09-10 14:40:51 +0200

    added more functions

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
 import os
-import random
 
 import PyPDF2
 import pyttsx3
@@ -13,12 +12,18 @@ def return_text_to_speech(text_input):
     speak.runAndWait()
 
 
-def open_file_path(file_input_path):
-    new_path = open(file_input_path, 'rb')
-    return new_path
+def open_file(file_input_path):
+    try:
+        new_path = open(file_input_path, 'rb')
+        success: bool = True
+        return new_path, success
+    except OSError as e:
+        print("open() failed", e)
+        success: bool = False
+        return success
 
 
-def read_file(file_to_read: str):
+def convert_from_pdf(file_to_read: str):
     pdf_reader = PyPDF2.PdfReader(file_to_read)
     # the page with which you want to start
     from_page = pdf_reader.pages[0]
@@ -26,16 +31,18 @@ def read_file(file_to_read: str):
     return text
 
 
-def save_converted_file(text_to_convert):
+def convert_from_text(file_input_path):
+    with open(file_input_path) as text_txt:
+        text = text_txt.read()
+        return text
+
+
+def save_converted_file(text_to_convert, file_name: str):
     # determine the path where the converted file should be saved to
     save_path: str = os.path.expanduser('~') + "\\Downloads\\"
     language: str = 'en'
-    random_int: int = random.randint(0, 200)
-    file_name = "converted_pdf_file" + str(random_int) + ".mp3"
-
     # convert the file into a TTS file
     converted_file = gTTS(text=text_to_convert, lang=language, slow=False)
-
     # save the file in the set path with the set name
     converted_file.save(save_path + file_name)
     print("File has been saved in: " + save_path + " as: " + file_name)
--- main.py
+++ main.py
@@ -1,51 +1,73 @@
+__author__ = "Jelle de Wolf"
+__credits__ = ["Jelle de Wolf"]
+__version__ = "1.0.0"
+__maintainer__ = "Jelle de Wolf"
+__email__ = "wolfjj@hva.nl"
+__status__ = "Production"
+"""Goal of the program:
+
+The goal of this simple program is to allow the user to convert either a PDF/TXT file and then,
+save the converted file to the downloads folder.
+
+The user us also able to listen to the converted file.
+"""
 import os
+import random
 import sys
 
 import functions
 
 exit_message: str = "Thanks for using the converter!"
 
+converted_files_list = []
+
+
+def convert_another_file():
+    another_file = input("Would you like to convert another file?")
+    if another_file == 'no'.lower():
+        print("Thanks for using the converter!")
+        sys.exit()
+    if another_file == 'yes'.lower():
+        # return the user to the start of the loop
+        start()
+    else:
+        print("Please enter 'yes' or 'no'!")
+        convert_another_file()
+
+
+def listen_to_converted_file(text_to_convert):
+    user_response = input("Would you like to listen to the converted file?")
+    if user_response == 'yes'.lower():
+        functions.return_text_to_speech(text_to_convert)
+        convert_another_file()
+    if user_response == 'no'.lower():
+        convert_another_file()
+    else:
+        print("Did not recognize input value")
+        listen_to_converted_file(text_to_convert)
+
 
 def start():
     while True:
+        random_int: int = random.randint(0, 200)
+        file_name = "converted_pdf_file" + str(random_int) + ".mp3"
+        converted_files_list.append(file_name)
+        selected_type = input("Which file type would you like to convert? Choose between PDF or TXT")
         user_file_path: str = input("Please enter path name in the following format: directory\\file_name")
-        # get the path where the file is located
-        file_path: str = os.path.expanduser('~') + "\\" + user_file_path
+        file_to_read: str = os.path.expanduser('~') + "\\" + user_file_path
+        if not functions.open_file(file_to_read):
+            start()
         save_file_bool = input("Would you like to save the converted file?")
         if not save_file_bool == 'yes'.lower():
             print(exit_message)
             sys.exit()
-        file_to_convert = functions.open_file_path(file_path)
-        text_to_convert = functions.read_file(file_to_convert)
-        functions.save_converted_file(text_to_convert)
-        another_file: str = input("Would you like to convert another PDF file?")
-        if not another_file == 'yes'.lower():
-            print(exit_message)
-            sys.exit()
-        # return the user to the start of the loop
-        start()
-
-    # try:
-    #     path = functions.open_file_path(file_path)
-    # except OSError as e:
-    #     print("open() failed", e)
-    # else:
-    #     with path:
-    #
-    #         text_speech_value = input("Type yes if you want to hear the text in the file...")
-    #         if text_speech_value in ["Yes", "yes"]:
-    #             # send text to function to read
-    #             text = functions.read_file(path)
-    #
-    #             # send text to function to speak
-    #             functions.return_text_to_speech(text)
-    #         if text_speech_value in ["No", "no"]:
-    #             print("User made the choice to not hear the text")
-    # save_converted_filetemp()
+        if selected_type == 'TXT'.lower():
+            text_to_convert = functions.convert_from_text(file_to_read)
+        else:
+            text_to_convert = functions.convert_from_pdf(file_to_read)
+        functions.save_converted_file(text_to_convert, file_name)
+        print(converted_files_list)
+        listen_to_converted_file(text_to_convert)
 
 
 start()
-
-# TODO: create a list of converted files and show that to the user
-# TODO: implement a txt file reader next to the PDF reader
-# TODO: use more dataTypes int,str,float
